2db9a16d0274e555a5ebd82adcbf6f59b59cfd6a
Raul Afonso <afonsoafn@gmail.com>
2026-05-05 15:38:36 +0100

    fix: adicionar import useEffect em fecho e kiosk checkin

--- src/pages/kiosk/KioskCheckinPage.tsx
+++ src/pages/kiosk/KioskCheckinPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { ApiError } from "../../lib/api";
 import { kioskScan, type KioskScanResult } from "../hr/hrApi";
